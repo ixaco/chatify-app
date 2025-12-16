1d3ade952cf7864f83b6b736d9174da02693b2a1
Ixtiyorjon Abdurahmonov <forlesson98@gmail.com>
2025-12-16 06:55:59 +0500

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+import authRouters from './routes/auth.route.js'
+import messageRouters from './routes/message.route.js'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use('/api/auth', authRouters)
+app.use('/api/messages', messageRouters)
+
+app.listen(PORT, () => console.log('Server is running on port:' + PORT))
