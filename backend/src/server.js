64a531b8297c7391d6a7a09958ec0771e07bfd7a
Ixtiyorjon Abdurahmonov <forlesson98@gmail.com>
2025-12-17 20:10:48 +0500

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 
+import { connectDB } from './lib/db.js'
 import authRouters from './routes/auth.route.js'
 import messageRouters from './routes/message.route.js'
 
@@ -10,7 +11,12 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.json()) // req.body
+
 app.use('/api/auth', authRouters)
 app.use('/api/messages', messageRouters)
 
-app.listen(PORT, () => console.log('Server is running on port:' + PORT))
+app.listen(PORT, () => {
+	console.log('Server is running on port:' + PORT)
+	connectDB()
+})
