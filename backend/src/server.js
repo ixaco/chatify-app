a58eb35170f612eccf275909550eb5a3bef803a6
Ixtiyorjon Abdurahmonov <forlesson98@gmail.com>
2025-12-27 16:32:21 +0500

    Message routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,8 @@ import express from 'express'
 
 import { connectDB } from './lib/db.js'
 import { ENV } from './lib/env.js'
-import authRouters from './routes/auth.route.js'
-import messageRouters from './routes/message.route.js'
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 
 const app = express()
 
@@ -13,8 +13,8 @@ const PORT = ENV.PORT || 3000
 app.use(express.json()) // req.body
 app.use(cookieParser())
 
-app.use('/api/auth', authRouters)
-app.use('/api/messages', messageRouters)
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 app.listen(PORT, () => {
 	console.log('Server is running on port:' + PORT)
