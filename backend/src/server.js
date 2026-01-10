19d2fc9e422e3193e365b3d7d99217aa83edc28f
Ixtiyorjon Abdurahmonov <forlesson98@gmail.com>
2026-01-10 19:07:28 +0500

    online users implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,20 @@ import express from 'express'
 
 import { connectDB } from './lib/db.js'
 import { ENV } from './lib/env.js'
+import { app, server } from './lib/socket.js'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 
-const app = express()
-
 const PORT = ENV.PORT || 3000
 
-app.use(express.json({ limit: '5mb' })) // req.body - 5MB gacha
-app.use(express.urlencoded({ limit: '5mb', extended: true })) // URL-encoded data uchun
-
+app.use(express.json({ limit: '5mb' })) // req.body // 5mb limit
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
-app.listen(PORT, () => {
-	console.log('Server is running on port:' + PORT)
+server.listen(PORT, () => {
+	console.log('Server running on port: ' + PORT)
 	connectDB()
 })
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import toast from 'react-hot-toast'
 import { io } from 'socket.io-client'
 import { create } from 'zustand'
-import { axiosInstance } from '../lib/axios'
+import { axiosInstance } from '../lib/axios.js'
 
 const BASE_URL =
 	import.meta.env.MODE === 'development' ? 'http://localhost:3000' : '/'
